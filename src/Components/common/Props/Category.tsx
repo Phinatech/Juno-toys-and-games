3dc4b4fe9ced2401926e9fba3303d3bff466266d
Phinatech <ugbojudithchinemerem@gmil.com>
2023-07-03 22:23:40 +0100

    new changes

--- src/Components/common/Props/Category.tsx
+++ src/Components/common/Props/Category.tsx
@@ -3,10 +3,10 @@ import { catergoryProps } from "../../../Types";
 
 const Category: React.FC<catergoryProps> = ({ subtitle, subtitle2 }) => {
   return (
-    <div className="w-[100%] h-[fit-content] flex justify-center bg-yellow-100">
+    <div className="w-[100%] h-[fit-content] flex justify-center ">
       <div className=" w-[80%] justify-center flex flex-col items-center gap-3 ">
         <div className="font-bold text-[#181D4E] ">{subtitle}</div>
-        <div className="text-[#181D4E] font-  text-[40px] max-md:text-[25px] max text-center font-bold leading-tight w-[100%]  bg-red-300">
+        <div className="text-[#181D4E] font-  text-[40px] max-md:text-[25px] max text-center font-bold leading-tight w-[100%]  ">
           {subtitle2}
         </div>
       </div>
